bbcaff09726dd176a537f066313f3143d835cecf
Rick Ortega <ricko@odusa.org>
2020-11-17 09:22:24 -0800

    added sys.getenv

--- test.R
+++ test.R
@@ -15,7 +15,7 @@ compose_email(
     subject = 'test 123',
     credentials = creds(
       user = '',
-      pass_envvar = smtp_pass,
+      pass_envvar = Sys.getenv(smtp_pass),
       provider = NULL,
       host = 'odusa-org.mail.protection.outlook.com',
       port = '',
