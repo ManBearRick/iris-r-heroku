d55f42ad07d7d064a93cd8c882c269863aea9fd7
Rick Ortega <ricko@odusa.org>
2020-11-17 08:53:41 -0800

    added blastula

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("salesforcer", "tidyverse")
+my_packages = c("salesforcer", "tidyverse", "blastula")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
--- test.R
+++ test.R
@@ -1,3 +1,25 @@
 library(tidyverse)
+library(blastula)
 
-"Hello" %>% print()
+compose_email(
+  header = md(
+    "<br>Test Email"),
+  body = md(c(
+    'body'
+  )),
+  footer = md(c('footer'))
+) %>%
+  smtp_send(
+    from = 'ricko@odusa.org',
+    to = 'ricko@odusa.org',
+    subject = 'test 123',
+    credentials = creds(
+      user = '',
+      pass_envvar = smtp_pass,
+      provider = NULL,
+      host = 'odusa-org.mail.protection.outlook.com',
+      port = '',
+      use_ssl = TRUE
+      
+    )
+  )
